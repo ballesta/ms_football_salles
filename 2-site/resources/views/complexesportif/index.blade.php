1dd0d5ac6c6ee71533a7715d04d8f0d7675445b1
Bernard BALLESTA <bernard@ballesta.fr>
2016-10-18 22:16:31 +0200

    Affiche hiérarchie en cours

--- 2-site/resources/views/complexesportif/index.blade.php
+++ 2-site/resources/views/complexesportif/index.blade.php
@@ -3,18 +3,24 @@
 {{--*/ usort($tableGrid, "SiteHelpers::_sort") /*--}}
 <div class="page-content row">
 <div class="page-content-wrapper m-t">
+    <div class="row">
+        <div class="col-md-3"><h4 class="pull-right">Réseau de salles</h4 ></div>
+        <div class="col-md-8"><h4 ><i><b>Bubble Soccer Arena</b></i></h4 ></div>
+    </div>
+
+</row>
 <div class="sbox">
 <div class="sbox-title">
 <div class="sbox-tools pull-left" >
-@if($access['is_add'] ==1)
-<a href="{{ URL::to('complexesportif/update?return='.$return) }}"
-class="tips"  title="{{ Lang::get('core.btn_create') }}">
-<i class="fa  fa-plus "></i> Créer</a>
+@if($access['is_add'] == 1)
+    <a href="{{ URL::to('complexesportif/update?return='.$return) }}"
+    class="tips"  title="{{ Lang::get('core.btn_create') }}">
+    <i class="fa  fa-plus "></i> Créer</a>
 @endif
 @if($access['is_remove'] ==1)
-<a href="javascript://ajax"  onclick="SximoDelete();" class="tips"
-title="Supprimer les lignes cochées">
-<i class="fa fa-trash-o"></i> Supprimer</a>
+    <a href="javascript://ajax"  onclick="SximoDelete();" class="tips"
+    title="Supprimer les lignes cochées">
+    <i class="fa fa-trash-o"></i> Supprimer</a>
 @endif
 <a href="{{ URL::to( 'complexesportif/search?return='.$return) }}"
 class="tips"
@@ -53,7 +59,7 @@ title="{{ Lang::get('core.btn_clearsearch') }}" >
 @endif
 @endif
 @endforeach
-<th width="40%"%"%"%"%"%"%"%"%"%"%"%"" ><span>{{ Lang::get('core.btn_action') }}</span></th>
+<th width="40%"><span>{{ Lang::get('core.btn_action') }}</span></th>
 </tr>
 </thead>
 <tbody>
