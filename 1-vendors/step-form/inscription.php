5555f61f96f3e5383cb502c2f1dfa34c6ab40d46
Bernard BALLESTA <bernard@ballesta.fr>
2016-12-02 01:57:29 +0100

    Inscription prototype extérieur OK / Suite: intégrer dans appli et connecter à la base

---
+++ 1-vendors/step-form/inscription.php
@@ -0,0 +1,9 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: bernard
+	 * Date: 01/12/2016
+	 * Time: 12:41
+	 */
+
+	echo "<h1>Inscription terminée-------</h1>";
---
+++ 1-vendors/step-form/step-form-wizard/inscription.php
@@ -0,0 +1,16 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: bernard
+	 * Date: 01/12/2016
+	 * Time: 12:41
+	 *
+	 *
+	 * Enregistrer le joueur si:
+	 *      La boite deroulante joueur est sélectionnée
+	 *      et
+	 *      Rien n'est rempli dans la fiche d'inscription d'un nouveau joueur
+	 *
+	 */
+
+	echo "<h1>Inscription terminée</h1>";
