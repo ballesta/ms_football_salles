d1ab2ae49cec8dc9427b43cf32216c2191f22d97
bernard <bernard@ballesta.fr>
2017-02-25 16:38:01 +0100

    Interface BSA

--- 2-site/app/Http/Controllers/FrontAccesStatistiquesController.php
+++ 2-site/app/Http/Controllers/FrontAccesStatistiquesController.php
@@ -17,6 +17,11 @@ class FrontAccesStatistiquesController extends Controller
     public function index()
     {
 	    return '<h1>Statistiques des parties</h1>
+				<p>Liste ds joueurs, des parties et accès aux statistiques pour chaque joueur et pour chaque équipe.
+				</p>
+				<p>En cours de développement
+				</p>
+				
                 <a href=http://football-bsa.ballesta.fr/index.php>
 					<h2>
 						Retour au site BSA
